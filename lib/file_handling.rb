302e98427353226254df7bceec6e60245b4a21b0
Daniel Baird <daniel@danielbaird.com>
2013-02-03 23:22:56 +1000

    make a test or two

--- lib/file_handling.rb
+++ lib/file_handling.rb
@@ -3,16 +3,16 @@
 
 class MCFS
 
-	def dir_exists? possible_dir
+	def self.dir_exists? possible_dir
 		File.directory? possible_dir
 	end
 
-	def safe_name fancy_name
+	def self.safe_name fancy_name
 		# return a super-safe filename by:
 		#  - replacing runs of whitespace or underscores with a dash
 		#  - removing everything else apart from letters, numbers, and dashes
 		#  - replacing runs of dashes with single dashes
 		#  - removing leading and trailing dashes
-		fancy_name.downcase.gsub(/(\w|\n|\r|_)+/, '-').gsub(/[^a-z0-9-]+/, '').gsub(/^-/, '').gsub(/-$/, '')
+		fancy_name.downcase.gsub(/(\s|\n|\r|_)+/, '-').gsub(/[^a-z0-9-]+/, '').gsub(/^-/, '').gsub(/-$/, '')
 	end
 end
--- tests/lib/file_handling_test.rb
+++ tests/lib/file_handling_test.rb
@@ -1,6 +1,30 @@
 
 require_relative '../../lib/file_handling'
 
-describe "dummy test" do
-	
+# -------------------------------------------------------------------
+describe 'safe_name' do
+	# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+	it "should let okay names pass through" do
+
+		okay_names = ['okay', 'has-dash', 'has-sev-er-al-dashes']
+		okay_names.each do |name|
+			MCFS.safe_name(name).should == name
+		end
+	end
+	# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+	it "should change whitespace into dashes" do
+
+		whitespace_names = [
+			['one space', 'one-space'],
+			['double  space', 'double-space'],
+			['two spa ces', 'two-spa-ces'],
+			['mixed  spa ces', 'mixed-spa-ces'],
+		]
+
+		whitespace_names.each do |name|
+			MCFS.safe_name(name[0]).should == name[1]
+		end
+	end
+	# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
 end
